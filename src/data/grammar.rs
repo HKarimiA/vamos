e94931e2e155635d37c50b80a630e406856fc992
Hossein Karimi <hossein.karimi@prodyna.com>
2026-02-15 14:01:54 +0100

    Add grammar content for "Determiners" topic with explanations and quiz questions

--- src/data/grammar.rs
+++ src/data/grammar.rs
@@ -213,6 +213,7 @@ pub fn load_grammar_content(topic_id: u32) -> Result<GrammarContent, String> {
         3 => include_str!("../../translations/grammar/3/en.json"),
         4 => include_str!("../../translations/grammar/4/en.json"),
         5 => include_str!("../../translations/grammar/5/en.json"),
+        6 => include_str!("../../translations/grammar/6/en.json"),
         _ => return Err(format!("Topic {} not yet available", topic_id)),
     };
 
